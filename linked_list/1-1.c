bbdae8ba81a8460bece7ff394bbacce7c690d9a8
GV_Dan <zxc61013@gmail.com>
2023-03-17 17:20:54 +0800

    新增Linked List建立及輸出位址、資料、Pointer

--- linked_list/1-1.c
+++ linked_list/1-1.c
@@ -6,3 +6,37 @@ struct node {
     struct node *next;
 };
 typedef struct node Node;
+
+int main() {
+    int i, val, num;
+    Node *first, *current, *previous;
+    printf("Number of nodes: ");
+    scanf("%d", &num);
+
+    for (i = 0; i < num; i++) {
+        // Create new Node
+        current = (Node*) malloc(sizeof(Node));
+        printf("Data for node %d : ", i + 1);
+        // Input the data for Node
+        scanf("%d", &(current -> data));
+        if (i == 0) {
+            // first and previous pointed to this Node
+            first = current;
+            previous = current;
+        }
+        else {
+            previous -> next = current;
+            current -> next = NULL;
+            previous = current;
+        }
+    }
+    current = first;
+    while (current != NULL) {
+        printf("address = %p, ", current);
+        printf("data = %d, ", current -> data);
+        printf("next = %p, \n", current -> next);
+        current = current -> next;
+    }
+
+    return 0;
+}
